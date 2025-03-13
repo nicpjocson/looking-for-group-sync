ce8308c740469d0e7c0a55500fcc93c0de4b389b
Nicole Jocson <nicpjocson@gmail.com>
2025-03-14 00:19:43 +0800

    Move party-related functions

--- Driver.cpp
+++ Driver.cpp
@@ -21,7 +21,7 @@ void Driver::run()
         for (Dungeon* dungeon : this->dungeons) {
 
             // At least one party looking for dungeon
-            if (this->partyQueue.size() > 0) {
+            if (QueueManager::getInstance()->getPartyQueue().size() > 0) {
                 dungeon->startDungeon();
             }
         }
@@ -44,69 +44,6 @@ void Driver::waitForThreadsToFinish()
             }
         }
     }
-}
-
-/*
-
-    Party-creation-related
-
-*/
-bool Driver::canCreateParty()
-{
-    // Less players in queue than players required for full party
-    if (this->tankQueue.size() < 1 ||
-        this->healerQueue.size() < 1 ||
-        this->dpsQueue.size() < 3) {
-        return false;
-    }
-
-    // There are enough players for full party
-    return true;
-}
-
-Party* Driver::createParty()
-{
-    // Assign 1 Tank to Party
-    Tank* newTank = this->tankQueue.front();
-    this->tankQueue.pop();
-
-    // Assign 1 Healer to Party
-    Healer* newHealer = this->healerQueue.front();
-    this->healerQueue.pop();
-
-    // Assign 3 DPS to Party
-    DPS* newDPS1 = this->dpsQueue.front();
-    this->dpsQueue.pop();
-    DPS* newDPS2 = this->dpsQueue.front();
-    this->dpsQueue.pop();
-    DPS* newDPS3 = this->dpsQueue.front();
-    this->dpsQueue.pop();
-
-    // DEBUG
-    std::cout << "created new party " << this->partyQueue.size() - 1 << std::endl;
-
-    Party* newParty = new Party(newTank, newHealer, newDPS1, newDPS2, newDPS3);
-
-    return newParty;
-}
-
-// Create as many full parties as possible
-void Driver::createParties()
-{
-    while (this->canCreateParty()) 
-    {
-        Party* newParty = this->createParty();
-        this->partyQueue.push(newParty);
-    }
-
-    this->handleLeftoverPlayers();
-}
-
-void Driver::handleLeftoverPlayers()
-{
-    this->leftoverTanks = this->tankQueue.size();
-    this->leftoverHealers = this->healerQueue.size();
-    this->leftoverDPS = this->dpsQueue.size();
 }
 
 /*
