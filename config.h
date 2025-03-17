f05bd9112bd0cfee55a4dbc7d076d2ad5259a2b2
Nicole Jocson <nicpjocson@gmail.com>
2025-03-18 06:29:37 +0800

    Add test case

--- config.h
+++ config.h
@@ -12,10 +12,16 @@
         (6) MAX_TIME       - max. time before an instance is finished
 */
 
+//const unsigned int MAX_INSTANCES = 4;
+//const unsigned int TANK_PLAYERS = 17;
+//const unsigned int HEALER_PLAYERS = 17;
+//const unsigned int DPS_PLAYERS = 51;
+//const unsigned int MIN_TIME = 1;
+//const unsigned int MAX_TIME = 5;
 const unsigned int MAX_INSTANCES = 4;
-const unsigned int TANK_PLAYERS = 17;
-const unsigned int HEALER_PLAYERS = 17;
-const unsigned int DPS_PLAYERS = 51;
+const unsigned int TANK_PLAYERS = 26;
+const unsigned int HEALER_PLAYERS = 26;
+const unsigned int DPS_PLAYERS = 78;
 const unsigned int MIN_TIME = 1;
 const unsigned int MAX_TIME = 5;
 
