bf18633e1ba86f0b987686f980e3cc26a604aa37
Nicole Jocson <nicpjocson@gmail.com>
2025-03-19 00:55:12 +0800

    Create own new input validation

--- config.h
+++ config.h
@@ -12,16 +12,30 @@
         (6) MAX_TIME       - max. time before an instance is finished
 */
 
+// no dungeons reused, whole parties, has leftover
+// 4 parties, leftover: 1 tank, 1 healer, 2 dps
 //const unsigned int MAX_INSTANCES = 4;
 //const unsigned int TANK_PLAYERS = 17;
 //const unsigned int HEALER_PLAYERS = 17;
-//const unsigned int DPS_PLAYERS = 51;
+//const unsigned int DPS_PLAYERS = 50;
 //const unsigned int MIN_TIME = 1;
 //const unsigned int MAX_TIME = 5;
+
+// dungeons reused, nonwhole parties, no leftover
+// 4 + 2 1/2 parties, 0 leftover
+//const unsigned int MAX_INSTANCES = 4;
+//const unsigned int TANK_PLAYERS = 26;
+//const unsigned int HEALER_PLAYERS = 26;
+//const unsigned int DPPLAYERS = 78;
+//const unsigned int MIN_TIME = 1;
+//const unsigned int MAX_TIME = 5;
+
+// dungeons reused, nonwhole parties, has leftover
+// 4 + 2 1/2 parties, leftover: 0 tank, 1 healer, 2 dps
 const unsigned int MAX_INSTANCES = 4;
 const unsigned int TANK_PLAYERS = 26;
-const unsigned int HEALER_PLAYERS = 26;
-const unsigned int DPS_PLAYERS = 78;
+const unsigned int HEALER_PLAYERS = 27;
+const unsigned int DPS_PLAYERS = 80;
 const unsigned int MIN_TIME = 1;
 const unsigned int MAX_TIME = 5;
 
